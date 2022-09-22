02496d741e68a7926809b02f1c18935fcc5cb4a6
OolongTea620 <rlafls9596@gmail.com>
2022-09-22 18:34:58 +0900

    day9

--- chapter_05/05_express.md
+++ chapter_05/05_express.md
@@ -4,6 +4,9 @@
 $ npm i express
 $ npm i -D nodemon
 ```
+  npm 라이브러리를 볼 때 
+  - 최근 활동이 1년 이내인 것
+  - 인기순(다운로드 많은 수)
 ### 2. 서버 작성하기 
 ```javascript
 const express = require("express");
@@ -22,4 +25,125 @@ app.get("/about", (req, res) => {
 app.listen(app.get("port"), () => {
   console.log("익스프레스 서버 실행");
 });
+```
+#### 2-1. **라우터로 응답 보내기**
+- html 파일 보내는 방식
+- json으로 보내는 방식
+### 3. 미들웨어 사용하기
+**미들웨어란?**
+라우터가 실행되기 전에 반드시 한번 실행되는 함수
+
+### 미들웨어의 순서또한 중요하다.
+### 3-1.미들웨어를 이용한 에러처리
+
+### 4. next  활용법
+1. 미들웨어 다음으로 
+2. 분기처리 때
+3. next(route) 인경우-> 같은 url을 가진 경우
+
+### 5. morgan, bodyParser, cookieParser 라이브러리
+- morgan  
+nodemom 실행시 어떤 요청이 들어왔는지 확인해주는 라이브러리
+- cookie-parser 
+body 데이터 처리를 도와주는 라이브러리
+### 5. express.static
+
+### 6. 멀티파트 데이터 형식
+  form 태그의 entype이 multipart/form-data인 경우
+  - body-parser로는 요청 본문을 해석할 수 없음
+  - multer 패키지 필요 
+**multer**
+npm install
+```zsh
+$npm i multer
+```
+### 7. dotenv
+중요한 정보를 환경 변수로 만들어 값을 가리는 역할
+
+
+### 8. express.Router
+app.js가 길어지는 것을 막을 수 있음
+
+- 라우트 매개변수
+**:id**를 넣으면 req.params.id로 받을 수 있음
+
+- 쿼리 스트링을 사용한 경우
+req.query."이름"으로 받을 수 있음
+
+**req**   
+-req.app : req 객체를 통해서 app 객체에 접근가능   
+  ex) req.app.get("port");  
+
+req.body : body-parser 미들웨어가 만드는 요청의 본문을 해석한 객체
+
+req.cookies: cookie-parser 미들웨어가 만드는 요청의 본문을 해석한 객체  
+
+req.ip :요청의 ip 주소가 담겨있다
+
+req.params : 라우트 매개변수에 대한 정보가 담긴 객체
+
+req.query : 쿼리스트링에 대한 정보가 담긴 객체
+
+req.signedCookies : 서명된 쿠키들은  req.cookies 대신 여기에 담겨 있다.
+
+req.get(헤더 이름) : 헤더의 값을 가져오고 싶을 때 사용하는 메서드
+
+### 9. 템플릿 엔진
+요즘은 잘 사용안해요.
+HTML의 정적인 단점을 개선
+- 반복문, 조건문, 변수 등을 사용할 수 있음
+- 동적인 페이지 작성가능
+- PHP, JSP와 유사
+
+
+#### 9-1. Pug(구 jade)
+
+문법이 Ruby와 비슷해 코등 양이 줄어듦
+HTML과 달리 호불호 명확   
+익스프레스에 app.set으로 퍼그 연결
+```javascript
+app.set("port", 3000);
+app.set("views", path.join(__dirname,"views")); // views 폴더(templete 폴더) 경로 지정
+app.set("view engine", "pug");
+app.use(morgan("dev"));
+```
+
+#### 9-2. Pug- 변수
+```javascript
+/**
+ * res.render에서 두 번째 인수 객체에 Pug변수 넣음   
+*/
+router.get("/", function(req,res,next) {
+  res.render("index", {title : "express"});
+});
+//res.locals 객체에 넣는 것도 가능(미들 웨어 간 공유)
+router.get("/", function(req,res,next) {
+  res.locals.title = "Express";
+  res.render("index");
+});
+
+```
+=이나 #{}으로 변수 렌더링 가능(= 뒤에는 자바스크립트 문법 사용가능)
+
+### 10. 넌적스
+Pug의 문법에 적용이 힘들면 넌적스를 사용하면 좋다
+```zsh
+$npm i nunjucks
+```
+- Pug를 지우고 Nunjucks 설치
+- 확장자는 html 혹은 nkj(view engine을 nkj로)
+
+```javascript
+const nunjucks = require("nunjucks");
+
+app.set("port", 3000);
+app.set("views", path.join(__dirname,"views")); 
+app.set("view engine", "html");
+nunjucks.cofigure("views", {
+  express : app,
+  watch : true,
+});
+
+app.use(morgan("dev"));
+
 ```
--- chapter_05/app.js
+++ chapter_05/app.js
@@ -1,15 +1,237 @@
+// .env 설정
+const dotenv = require("dotenv");
+
 const express = require("express");
+const path = require("path");
+//서버 로깅
+const morgan = require("morgan");
+//
+const cookieParser = require("cookie-parser");
+
+//세션
+const session = require("express-session");
+
+// 멀티파트
+const multer = require("multer");
+const fs = require("fs");
+
+dotenv.config();
+// 라우터 분리
+const indexRouter = require("./routes"); //  잠시 분리했다가 합쳐주고
+const userRouter = require("./routes/user");
+const router = require("./routes");
+
 const app = express();
 
-app.set("port", process.env.PORT || 3000); // 서버에 'port'라는 속성(전역변수)을 준다
-app.get("/", (req, res) => {
-  res.send("hello express");
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("upload 폴더가 없어서 upload 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    //업로드된 파일이 어디에 저장되는가?(디스크, 메모리, s3, googelcloud 선택 가능)
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname); // 확장자를 포함한 이름
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: {
+    // 파일 사이즈 제한 5메가 바이트 아래만 전송가능 초과시 400 에러 발생
+    fileSize: 5 * 1024 * 1024,
+  },
 });
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+app.post("/upload", upload.single("image"), (req, res) => {
+  // 한 라우터에 만 미들웨어 적용
+  console.log(req.file);
+  res.send("ok");
+});
+//app 관련 설정
+app.set("port", process.env.PORT || 3000); // 서버에 'port'라는 속성(전역변수)을 준다
+/**
+ * app.http 메소드 를 쓰는 방식으로 호출 할 수 있다.
+ */
+
+// 미들웨어 : 동작 순서가 준비하다.
+// morgan 사용
+//배포시
+// app.use(morgan("combined"));
+
+//개발시
+app.use(morgan("dev"));
+
+// app.use("요청경로", express.static("실제경로"));
+// 목적 : 보안, 서버의 파일 구조를 예측할 수 없음
+// localhost:3000/zerocho.html        learn-express/public/zerocho.html
+// localhost:3000/hello.css        learn-express/public/hello.css
+
+app.use("/", (req, res, next) => {
+  if (req.session) {
+    //로그인 시 사진을 전송할 것이고 아니면 그냥 다음으로 넘어가고
+    express.static(path.join(__dirname, "public"))(req, res, next);
+    //확장된 미들웨어 작성법
+  } else {
+    next();
+  }
+});
+
+//cookie-parser
+app.use(cookieParser(process.env.COOKIE_SECRET));
+// body parsing 의 기능
+app.use(express.json()); // body를 json으로 보내줌
+// extended 가 true면 qs , false면 querySting 모듈 사용
+app.use(express.urlencoded({ extended: true })); // form  형식의 데이터를 처리
+app.use(
+  session({
+    resave: false, // 요청이 왔을 때 세션에 수정상황이 생기지 않도록 다시 저장할지 여부
+    saveUninitialized: false, // 세션에 저장할 냉역이 없더라도 세션을 저장할 지
+    secret: "zerochopassword", // 세션 암호 비밀번호
+    cookie: {
+      httpOnly: true,
+    },
+    name: "connect.sid",
+  })
+);
+// 만약  데이터 이미지를 처리하기 위해서는? >> 그건 다음장에
+app.use(multer().array());
+// 공통 미들웨어
+app.use((req, res, next) => {
+  // 미들웨어 : 모든 요청에 공통적으로 실행된다.
+  console.log("모든 요청에 실행하고 싶어요");
+  next(); // 미들웨어는 다음과 같이 해줘야 다음으로 넘어갈 수 있다.
+});
+app.use("/about", (req, res, next) => {
+  console.log("/about에서만 실행이 된다.");
+  req.data = "zerocho비번";
+});
+
+app.use(
+  //여러개의 미들웨어를 스크립트 순차적으로 실행시킬 수 있다.
+  (req, res, next) => {
+    console.log("1 요청에 실행하고 싶어요");
+    req.data; // zerocho 비번
+    next();
+  },
+  (req, res, next) => {
+    console.log("2 요청에 실행하고 싶어요");
+    next(); // next에 인수가 없으면 다음 미들웨어로 넘어가고
+  },
+  (req, res, next) => {
+    try {
+      next();
+    } catch (error) {
+      next(error); //next에 인수가 있다면 아래에 적어둔 에러처리 미들웨어로 넘어간다.
+    }
+  }
+);
+
+// 라우터들 (범위가 넓을 수록 뒤로 보낸다)
+// app.get("/", (req, res) => {
+//   //1. 여러 자원  send 2번 이상 보내면 안됨
+//   // res.writeHead(200, { "Content-Type": "text/plain" });
+//   // res.send("hello express");
+//   // // 해당 2줄을 다음과 같이 한 줄로 줄일 수 있다.
+//   res.status(200).send("hello express");
+
+//   //res.sendFile(path.join(__dirname, "index.html"));
+//   // res.json("hello rezocho");
+// });
+app.use("/", indexRouter);
+app.use("/user", userRouter);
 app.post("/", (req, res) => {
-  res.send("hello express");
+  res.send("hello express1");
+});
+
+app.get("category/:name", (req, res) => {
+  res.send("test");
 });
 app.get("/about", (req, res) => {
-  res.send("hello express");
+  res.send("hello express2");
+});
+app.get("/json", (req, res) => {
+  //json으로 응답보내기
+  //res.writeHead(200, { "Content-Type": "application/json" });
+  //res.end(JSON.stringify({ hello: "hyelin" }));
+
+  res.json({ hello: "hyelin" });
+});
+
+app.get(
+  "/route",
+  (req, res, next) => {
+    //1
+    res.send("next가 실행되나요?");
+    next("route"); //1번과 3번 실행된다.
+  },
+  (req, res) => {
+    //2
+    console.log("다음 같은 url route가 실행될까?");
+  }
+);
+
+app.get("/route", (req, res) => {
+  //3
+  console.log("실행됩니다");
+});
+
+app.get("/cookieParser", (res, req) => {
+  req.cookies;
+  //req.signedCookies;
+  res.cookie("name", encodeURIComponent(name), {
+    expire: new Date(),
+    httpOnly: true,
+    path: "/",
+  });
+  res.send("hello cookie");
+});
+
+app.post("/body", (req, res, next) => {
+  //
+  req.body;
+});
+
+app.get("/session", (req, res) => {
+  req.session.id = "hello"; // 요청을 보낸 클라이언트만 hello
+});
+app.get("*", (req, res) => {
+  res.send("hello wildcard");
+});
+
+// 라우터 그룹화 하기
+// app.get("/group", (req, res) => {
+//   res.send("hello groupGet");
+// });
+// app.post("/group", (req, res) => {
+//   res.send("hello groupPost");
+// });
+router
+  .route("/group")
+  .get((req, res) => {
+    res.send("hello groupGet");
+  })
+  .post((req, res) => {
+    res.send("hello groupPost");
+  });
+/**
+ * 에러 처리 미들웨어
+ * 반드시 매개변수는 4개 여야 한다.
+ */
+
+app.use((req, res, next) => {
+  res.status(404).send("404에러는 여기에 처리");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("에러났지 하지만 알알랴줌");
 });
 app.listen(app.get("port"), () => {
   console.log("익스프레스 서버 실행");
